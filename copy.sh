116ed0a7c45f694e7b6f185eccae4056d2787d57
seb <seb@hosting.marsat>
2023-12-08 12:24:28 +0100

    - copy design : pour le mode unifié on ne copie rien puisque les css et js sont directement dans le répertoire sortie qui correspond au répertoire source mais non dans .design

--- copy.sh
+++ copy.sh
@@ -2,7 +2,8 @@ CONF=$(cat /documents/.command/.configuration-asciidoctor)
 if [[ "${CONF}" == *"/output"* ]]; then
     echo output
     copy-distinct.sh
-else
-    echo no output
-    copy-unified.sh
 fi 
+#else
+#    echo no output
+#    copy-unified.sh
+#fi 
